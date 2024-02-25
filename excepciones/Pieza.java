f2f11dd20503dde6626321d470a113151fee6b2d
Diego Muñoz Arribas <dimuarr2002@gmail.com>
2024-02-25 22:31:14 +0100

    Ejercicio 5 | Excepciones [3]

--- excepciones/Pieza.java
+++ excepciones/Pieza.java
@@ -2,22 +2,28 @@ public class Pieza {
     private String forma;
     private String color;
 
-    
-    
     public Pieza(String forma, String color) {
         this.forma = forma;
         this.color = color;
     }
 
-    public double area(){
+    public double area() {
         return 0.0;
     }
 
     public boolean equals(Object o) {
-        Pieza p = (Pieza) o;
-        return this.color.equals(p.color) &&
-        this.forma.equals(p.forma) &&
-        this.area() == p.area();
+        Pieza p;
+        try {
+            p = (Pieza) o;
+
+        } catch (ClassCastException e) {
+            return false;
         }
+
+        return this.color.equals(p.color) &&
+                this.forma.equals(p.forma) &&
+                this.area() == p.area();
         
+    }
+
 }
--- excepciones/ejer5.java
+++ excepciones/ejer5.java
@@ -7,10 +7,10 @@ public class ejer5 {
         boolean b1 = p1.equals(p2);
         boolean b2 = d.equals(k);
         boolean b3 = k.equals(p2);
-        boolean b4 = p1.equals(d);  
-        System.out.println("b1: " + b1); 
+        boolean b4 = p1.equals(d);
+        System.out.println("b1: " + b1);
         System.out.println("b2: " + b2);
-        System.out.println("b3: " + b3); 
+        System.out.println("b3: " + b3);
         System.out.println("b4: " + b4);
     }
 }
