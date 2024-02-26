5b640375dc5907ced9a3f7ef364a8aeeb732e1a7
Diego Muñoz Arribas <dimuarr2002@gmail.com>
2024-02-26 18:07:55 +0100

    Ficheros prueba [0]

---
+++ ficheros/ejercicios1/rmj.java
@@ -0,0 +1,19 @@
+
+
+import java.io.File;
+
+public class rmj {
+
+    public static void main(String[] args) {
+        
+        try {
+        File file = new File(args[0]);
+
+           
+        } catch (Exception e) {
+           System.out.println("hola");
+        }
+
+
+    }
+}
