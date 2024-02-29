8afd46446d3323f10781718144e3a128a0a3e67b
Diego Muñoz Arribas <dimuarr2002@gmail.com>
2024-02-29 20:36:01 +0100

    Fichero ejercicio2 [1]

--- ficheros/ejercicios2/ejer1.java
+++ ficheros/ejercicios2/ejer1.java
@@ -9,29 +9,44 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ejer1 {
+
+    public static void limpiarPantalla() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static void main(String[] args) {
 
-String nose ;  
-String fichero;  
+        String linea;
+        String fichero;
+
         try {
 
-            
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Intoduce un nombre para el fichero");
-            
+            do {
+                System.out.println("Introduce un nombre para el fichero");
+                fichero = br.readLine();
+                limpiarPantalla();
+            } while (fichero.equals(""));
 
-            FileWriter in = new FileWriter("hola.txt");
-          
-           nose =br.readLine();
-        
-            while (!nose.equals("FIN")) {
-               
-                nose = br.readLine();
-                
+            fichero += ".txt";
+
+            FileWriter in = new FileWriter(fichero);
+            System.out.println("Escribe en el fichero: (fin para acabar)");
+            linea = br.readLine();
+
+            while (!linea.equals("fin")) {
+                in.write(linea + "\n");
+                in.flush();
+                linea = br.readLine();
+
             }
+            br.close();
             in.close();
-           
-        } catch (FileNotFoundException e1) {
+
+        } catch (
+
+        FileNotFoundException e1) {
             System.err.println("Error: No se encuentra el fichero");
         } catch (IOException e2) {
             System.err.println("Error leyendo/escribiendo fichero");
